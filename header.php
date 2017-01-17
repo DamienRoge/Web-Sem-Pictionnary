e3b07f4f00be67650b8f791db98abff76592823c
Damien Rogé <damien.roge.info@gmail.com>
2017-01-17 23:55:50 +0100

    Header
    Salue le joueur par son nom si il est co sinon « invité » + profile pic
    ou image d’invité

--- header.php
+++ header.php
@@ -1,19 +1,49 @@
 <?php
 session_start();
 ?>
-<header>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=utf-8 />
+    <link rel="stylesheet" media="screen" href="css/styles.css" >
 
-<?php
+    <link rel="stylesheet" media="screen" href="css/bootstrap.css" >
+    <link rel="stylesheet" media="screen" href="css/bootstrap.css.map" >
 
-if(isset($_SESSION['prenom'])){
-    echo "Bonjour ".$_SESSION['prenom']."<br>";
-    echo "<a href='logout.php'>Deconnexion</a>";
-}
-else{
-    echo "Bonjour invité";
-}
+    <link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAARGFwAP///wCEweMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADMiIzAAAAAAMiIiMAAAADMyAgIzMAAAEwAgIAMQAAAwAAIAADAAABAAAAAAEAAAAzMTEzMAAAADMAAAAwAAAAMAAAAzAAAAAwAAAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//wAA//8AAPgPAADwBwAA4AMAAMABAADAAQAAwAEAAMABAADgAwAA4AMAAOHDAADj4wAA9/cAAP//AAD//wAA" rel="icon" type="image/x-icon" />
+    <title>Pictionnary - Inscription</title>
+</head>
+<body>
 
-?>
-<canvas id="preview" width="0" height="0"></canvas>
+<header style="margin: 1em; height: 80px">
+
+    <?php
+
+    if(isset($_SESSION['prenom'])){
+        echo "<div class='header_user_infos' >";
+        echo "Bonjour ".$_SESSION['prenom']."<br>";
+
+        echo "<img id='profilepic' src='".$_SESSION['profilepic']."' ></img>";
+        echo "</div>";
+
+        echo "<div class='header_deco' >";
+        echo "<a  style='margin-top : 50px' href='logout.php'>Deconnexion</a>";
+        echo "</div>";
+    }
+    else{
+        echo "<div class='header_user_infos' >";
+        echo "Bonjour invité<br>";
+        echo "<img style='width :50px;height:50px' id='profilepic' src='css/images/invite.png' >";
+
+        echo "</div>";
+
+        echo "<div class='header_deco' >";
+        echo "<a style='display:block' href='login.php'>Se connecter</a>";
+        echo "</div>";
+
+    }
+
+    ?>
 
 </header>
+<hr>
